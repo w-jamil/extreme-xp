92a89334be890ae56028abec4642f500e8058575
w-jamil <dr.wjamil@outlook.com>
2025-08-01 14:08:43 +0100

    Added more cases.

--- master-experiment-suite/cl_case2/src/data_preprocessor.py
+++ master-experiment-suite/cl_case2/src/data_preprocessor.py
@@ -35,7 +35,7 @@ class DataPreprocessor:
         x_crypto = crypto.groupby(['timestamp']).sum()
 
         # Create target variable y (-1 for normal, 1 for attack)
-        y_crypto = x_crypto["label"].map({0: -1, 1: 1, 2: 1, 3: 1, 4: 1}).fillna(-1)
+        y_crypto = x_crypto["label"].map({0: -1, 1: 1, 2: 1, 3: 1, 4: 1,5: 1,6: 1,7: 1,8:1,9:1,10:1,11:1,12:1,13:1,14:1,15:1,16:1,17:1,18:1,19:1,20:1}).fillna(-1)
         
         # Create feature matrix X
         x_crypto = x_crypto.drop(columns=['label'], axis=1)
--- master-experiment-suite/online/src/online.py
+++ master-experiment-suite/online/src/online.py
@@ -298,7 +298,7 @@ def load_and_process_data(file_path):
         df = df.drop(columns=['user_id'], axis=1)
         
     x_df = df.groupby(['timestamp']).sum()
-    y_series = x_df["label"].map({0: -1, 1: 1, 2: 1, 3: 1, 4: 1}).fillna(-1)
+    y_series = x_df["label"].map({0: -1, 1: 1, 2: 1, 3: 1, 4: 1,5: 1,6: 1,7: 1,8:1,9:1,10:1,11:1,12:1,13:1,14:1,15:1,16:1,17:1,18:1,19:1,20:1}).fillna(-1)
     x_df = x_df.drop(columns=['label'], axis=1)
     
 
